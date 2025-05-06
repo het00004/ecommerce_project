8dcae675eb3aaf8dbc6328febb1c5135e28229e8
het00004 <hetchaudhari010105@gmail.com>
2025-05-06 17:26:19 +0530

    4

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -75,12 +75,20 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
 
 # Password validation
